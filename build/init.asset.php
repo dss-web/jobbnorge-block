1782a13403a0233f6c8f1c9c7c5dbe116ee05340
Per Søderlind <per@soderlind.no>
2023-01-08 02:47:44 +0100

    Update

---
+++ build/init.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render', 'wp-url'), 'version' => 'e89148c8505f48779b21');
