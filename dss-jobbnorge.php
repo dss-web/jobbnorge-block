b1d9bd7dd248edc23429cf6a9ddc832c7afe945f
Per Søderlind <per@soderlind.no>
2023-01-08 15:58:05 +0100

    Bump version

--- dss-jobbnorge.php
+++ dss-jobbnorge.php
@@ -5,7 +5,7 @@
  * Description:       Viser jobber fra jobbnorge.no
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            PerS
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
