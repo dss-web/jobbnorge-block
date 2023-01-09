d10ee5835ad57b6dd35b2fd931d02581ba7feb65
Per Søderlind <per@soderlind.no>
2023-01-09 22:09:10 +0100

    1.0.6

--- build/init.asset.php
+++ build/init.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render', 'wp-url'), 'version' => 'bc0fd427e769211ca269');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render', 'wp-url'), 'version' => '3c89d80c139242e0b043');
