85535bb4c9bca54e8f7de2e921e6cdfaedb515b4
Per Søderlind <per@soderlind.no>
2023-11-16 16:41:25 +0100

    bump version

--- build/init.asset.php
+++ build/init.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'ac2b277add806193da89');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '3c9759858397f49c3bd0');
