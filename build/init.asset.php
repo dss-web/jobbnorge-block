163b215e2367863c11fb70c78b7bded64158b7aa
Per Søderlind <per@soderlind.no>
2023-11-16 14:37:49 +0100

    Rebuild

--- build/init.asset.php
+++ build/init.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'b8531368f8265de3de48');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'ac2b277add806193da89');
